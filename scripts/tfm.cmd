bbe77e445afc8ec894c8c3d874ab66cc7949c05c
tescales <toby.scales@microsoft.com>
2019-07-15 11:03:24 -0700

    add error-checking to tfm.cmd

--- scripts/tfm.cmd
+++ scripts/tfm.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 set in_dir=%cd%
-set config_dir=%~dp0..\
+set root_dir=%~dp0..\
 set _psrun=0
 set _psinit=0
 set tfargs=
@@ -10,20 +10,27 @@ REM TODO: IF NOT EXIST secrets.tfvars RUN INITIALIZE_DEVPC
 for %%a in (.) do set currentfolder=%%~nxa
 set subid=%currentfolder:~0,6%
 
+rem Fix this to allow for running in rg folders
+:: echo %subid%|findstr /R /C:"\<[s][0123-9aAb-Cd-EfF][0123-9aAb-Cd-EfF][0123-9aAb-Cd-EfF][0123-9aAb-Cd-EfF][0123-9aAb-Cd-EfF]*" >nul
+:: if %errorlevel% NEQ 0 goto err_path
+
+
+if /i "%1"=="" goto help
 if /i "%1"=="init" set _psinit=1
 
 if /i "%1"=="plan" set _psrun=1
 if /i "%1"=="apply" set _psrun=1
 if /i "%1"=="destroy" set _psrun=1
 
-
 if "%_psinit%"=="1" (
-    pwsh -NoProfile -InputFormat None -ExecutionPolicy Bypass -file "%config_dir%tools\get-backend.ps1" 
+    pwsh -NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -file "%root_dir%scripts\get-backend.ps1"
+    if errorlevel == 1 goto cleanup
     set tfargs=-backend-config=^"backend.tfvars^"
 )
 
 if "%_psrun%"=="1" (
-    pwsh -NoProfile -InputFormat None -ExecutionPolicy Bypass -file "%config_dir%tools\get-config.ps1" 
+    pwsh -NoProfile -ExecutionPolicy Bypass -file "%root_dir%scripts\get-config.ps1" 
+    if errorlevel == 1 goto cleanup
 )
 
 rem .terraform directory exclusion code cribbed from https://stackoverflow.com/posts/25539569/revisions
@@ -35,15 +42,29 @@ for /R "%in_dir%" %%G in (.) DO (
         if "%%G"=="%in_dir%\." ( 
             echo >nul 
         ) else (
-            terraform %* -input=false %tfargs%
+            terraform %* %tfargs%
             popd 
         )
     )
 )
 
+goto :cleanup
+
+:help
+terraform /? 2>nul
+if %errorlevel% NEQ 127 echo ***** ERROR ***** & echo.  Is terraform in your path? & echo ***** ERROR *****
+goto :eof
+
+:cleanup
 rem delete-all config files for subscription-level deployments
 del /Q /s "%in_dir%\backend.tfvars" >nul 2>&1
-del /Q /s "%in_dir%\secrets.auto.tfvars" >nul
-del /Q /s "%in_dir%\globals.auto.tfvars" >nul
-del /Q /s "%in_dir%\provider.tf" >nul
-del /Q /s "%in_dir%\*.pfx" >nul
+del /Q /s "%in_dir%\secrets.auto.tfvars" >nul 2>&1
+del /Q /s "%in_dir%\globals.auto.tfvars" >nul 2>&1
+del /Q /s "%in_dir%\provider.tf" >nul 2>&1
+del /Q /s "%in_dir%\*.pfx" >nul 2>&1
+goto :eof
+
+:err_path
+echo You must run tfm.cmd from a path below the /live directory.
+
+:eof
