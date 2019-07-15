561065a6b3bcc473035704f8e8b5e46952c5baaa
tescales <toby.scales@microsoft.com>
2019-07-15 15:09:48 -0700

    fixup bootstrap.ps1

--- bootstrap/bootstrap.ps1
+++ bootstrap/bootstrap.ps1
@@ -52,25 +52,35 @@ import-module az.accounts
 import-module az.keyvault
 import-module az.storage
 
-$startPath = (get-item $PSScriptRoot).Parent.FullName
-$configPath = (join-path $startPath "config")
-$scriptPath = (join-path $startPath "scripts")
-$bootstrapPath = (join-path $startPath "bootstrap")
+$startPath = $pwd.path
+$rootPath = "$($startPath.Substring(0, $startPath.indexof("live")))"
+#$startPath = (get-item $PSScriptRoot).Parent.FullName
+$configPath = (join-path $rootPath "config")
+$scriptPath = (join-path $rootPath "scripts")
+$bootstrapPath = (join-path $rootPath "bootstrap")
 
 #deploy Ring0 resources
 $ring0ctx = Get-UserInputList (Get-AzContext -ListAvailable) -message "Select your Ring 0 Subscription"
-Set-AzContext -Context $ring0ctx[1]
+Set-AzContext -Context $ring0ctx[-1]
+
 $ring0loc = Get-UserInputList (Get-AzLocation) -message "Select an Azure Region to deploy Ring0 Resources"
 $ring0rg = Get-UserInputWithConfirmation -message "Enter a name for your Ring 0 resource group"
-New-AzResourceGroup -Name $ring0rg[1] -Location $($ring0loc.Location)
+New-AzResourceGroup -Name $ring0rg[-1] -Location $($ring0loc[-1].Location)
 New-AzResourceGroupDeployment -TemplateFile (join-path $bootstrapPath ring0.json) -TemplateParameterFile (join-path $bootstrapPath ring0.parameters.json) -ResourceGroupName $ring0rg[1] -AsJob
 
 #deploy Ring1 resources
 $ring1ctx = Get-UserInputList (Get-AzContext -ListAvailable) -message "Select your Ring 1 Subscription"
-Set-AzContext -Context $ring1ctx[1]
+
+$ring1Subscription = $ring1ctx[-1].Subscription 
+if (-not $ring1Subscription) { write-error "No Subscriptions found." -ErrorAction Stop }
+write-host -ForegroundColor yellow "Connecting to subscription $($ring1Subscription.name)..."
+Set-AzContext -Context $ring1ctx[-1]
+
+$storage_accts = Get-AzStorageAccount | sort-object -Property ResourceGroupName
+
 $ring1loc = Get-UserInputList (Get-AzLocation) -message "Select an Azure Region to deploy Ring1 Resources"
 $ring1rg = Get-UserInputWithConfirmation -message "Enter a name for your Ring 1 resource group"
-New-AzResourceGroup -Name $ring1rg[1] -Location $($ring1loc.Location)
+New-AzResourceGroup -Name $ring1rg[-1] -Location $($ring1loc[-1].Location)
 New-AzResourceGroupDeployment -TemplateFile (join-path $bootstrapPath ring1.json) -TemplateParameterFile (join-path $bootstrapPath ring1.parameters.json) -ResourceGroupName $ring1rg[1] -AsJob
 
 $ring0json = Get-content -raw (join-path $bootstrapPath ring0.parameters.json) | ConvertFrom-Json
@@ -79,37 +89,19 @@ $ring1json = Get-content -raw (join-path $bootstrapPath ring1.parameters.json) |
 $ring0KeyVaultName = $ring0json.parameters.keyVaultName.value
 $ring1KeyVaultName = $ring1json.parameters.keyVaultName.value
 
-Set-AzKeyVaultAccessPolicy -VaultName $ring0KeyVaultName -UserPrincipalName $ring0ctx.Account.Id -PermissionsToCertificates get,list,delete,create,import,update 
+Set-AzKeyVaultAccessPolicy -VaultName $ring0KeyVaultName -UserPrincipalName $ring0ctx[-1].Account.Id -PermissionsToCertificates get,list,delete,create,import,update 
+
 #TODO: add ability to select multiple subscriptions at once?
 #TODO: add subscription creation option?
 #TODO: use pure Terraform for cert creation?
-#$subs = Get-AzSubscription
-#$vaults = Get-AzKeyVault
-$storage_accts = Get-AzStorageAccount
 
-#$ring0Subscription = $ring0ctx.Subscription ##Get-UserInputList $subs "Choose the Ring 0 Subscription"
-#if (-not $ring0Subscription) { write-error "No Subscriptions found." -ErrorAction Stop }
-#write-host -ForegroundColor yellow "Connecting to subscription $($ring0Subscription.name)..."
-
-#set-azcontext -SubscriptionObject $ring0Subscription > $null
-
-#$ring0KeyVault = Get-UserInputList $vaults "Choose the Ring 0 Key Vault"
-#if (-not $ring0KeyVault) { write-error "No Keyvaults found." -ErrorAction Stop }
-
-$targetSubscription =  $ring1ctx.Subscription ##Get-UserInputList $subs "Choose the Target Subscription"
-if (-not $targetSubscription) { write-error "No Subscriptions found." -ErrorAction Stop }
-write-host -ForegroundColor yellow "Connecting to subscription $($targetSubscription.name)..."
-
-#$ring1KeyVault = Get-UserInputList $vaults "Choose the Ring 1 Key Vault for $($targetSubscription.name)"
-#if (-not $ring1KeyVault) { write-error "No Keyvaults found." -ErrorAction Stop }
-
-$tfStorageAccount = Get-UserInputList $storage_accts "Choose the Terraform State file Storage Account for $($targetSubscription.name)"
+$tfStorageAccount = Get-UserInputList $storage_accts "Choose the Terraform State file Storage Account for $($ring1Subscription.name)"
 if (-not $tfStorageAccount) { write-error "No Storage Accounts found." -ErrorAction Stop }
 
-$userName = Get-UserInputWithConfirmation "Enter the username to configure for $($targetSubscription.name)"
+$userName = Get-UserInputWithConfirmation "Enter the username to configure for $($ring1Subscription.name)"
 
-[string]$targetSubscriptionId = $targetSubscription.SubscriptionId
-[string]$TFStorageAccountName = $tfStorageAccount.StorageAccountName
+$targetSubscriptionId = $ring1Subscription.SubscriptionId
+$TFStorageAccountName = $tfStorageAccount[-1].StorageAccountName
 
 $EDOFargs= @{
     Username = $userName
