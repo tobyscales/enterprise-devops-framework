77e7dfcdd87c6d029a27d4b9c66c3bbd1104cb78
tescales <toby.scales@microsoft.com>
2019-07-15 11:05:31 -0700

    major update: provider.tf using maps

--- config/provider.tf
+++ config/provider.tf
@@ -1,14 +1,16 @@
-terraform {
-  #backend "azurerm" {}
-}
+#terraform {
+#backend "azurerm" {}
+#}
 provider azurerm {
-  client_id     = "${var.this-config["client_id"]}"
-  client_certificate_path = "${var.this-config["subalias"]}.${var.username}.pfx"
+  client_id                   = "${var.this-config["client_id"]}"
+  client_certificate_path     = "${var.this-config["subalias"]}.${var.username}.pfx"
   client_certificate_password = "${var.this-certpass}"
-  subscription_id = "${var.this-config["subscription_id"]}"
-  tenant_id       = "${var.this-config["tenant_id"]}"
-  version = ">= 1.0.0"
+  subscription_id             = "${var.this-config["subscription_id"]}"
+  tenant_id                   = "${var.this-config["tenant_id"]}"
+  version                     = ">= 1.0.0"
 }
-variable "username" { }
+variable "username" {}
 variable "this-certpass" {}
-variable "this-config" { type="map" }
+variable "this-config" {
+  type = "map"
+}
